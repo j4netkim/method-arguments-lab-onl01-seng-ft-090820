5a30ec4f2e4a8347a6875e4f4fbff6ac8e5bb9fb
J K <kj688895@gmail.com>
2020-09-10 01:36:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,3 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
-end
-
-introduction("Josh")
-
-def introduction_with_language(name, language)
+def introduction_with_language(name, lanuage)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-
-introduction_with_language("Dan", "Ember.js")
